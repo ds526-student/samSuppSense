e250bcb1cad2eb4e495b97e5a22db2f1814f2e1a
ds526-student <ds526@students.waikato.ac.nz>
2025-05-07 20:01:26 +1200

    Data transfer barcode->db->webpage successful

--- Backend/Server/routes.js
+++ Backend/Server/routes.js
@@ -1,18 +1,32 @@
-//importing necessary modules
 const express = require('express');
 const router = express.Router();
-//const db = require('/db');
+const mysql = require('mysql');
 
-
-// Handle submission of input
-router.post('/submit', (req, res) => {
-  const { userInput } = req.body;
-  storedInput = userInput;
+// Database connection
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "mcdonaldstest"
 });
 
-// Serve back stored input
-router.get('/result', (req, res) => {
-  res.json({ result: storedInput });
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Database connected in routes.js!");
+});
+
+// Handle submission of barcode and fetch product
+router.post('/query', (req, res) => {
+  const { barcode } = req.body;
+  const sql = 'SELECT * FROM products WHERE ProductID = ?';
+  con.query(sql, [barcode], (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error executing query');
+    } else {
+      res.json(result);
+    }
+  });
 });
 
 module.exports = router;
