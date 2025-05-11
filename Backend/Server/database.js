2c163306e782cc55aa651cc993282ff0e282fcf7
Jeff Jia <jarasta.doee@gmail.com>
2025-05-11 12:29:59 +1200

    updated files in branch from main

--- Backend/Server/database.js
+++ Backend/Server/database.js
@@ -29,6 +29,47 @@ router.post('/productSelect', (req, res) => {
   });
 });
 
+// uses the barcode from the frontend to select an ingredient from the database
+router.post('/ingredientSelect', (req, res) => {
+  const { barcode } = req.body;
+    con.query('SELECT IngredientID, IngredientName FROM ingredients WHERE IngredientID = ?', [barcode], (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error executing query');
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+// uses the product name from the frontend to select a product id from the database
+router.post('/getProductId', (req, res) => {
+  const { productName } = req.body;
+  console.log('Product Name:', productName);
+  con.query('SELECT ProductID From products WHERE ProductName = ?', [productName], (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error executing query');
+    } else {
+      res.json(result);
+      console.log('Product ID:', result);
+    }
+  });
+});
+
+router.post('/getIngredientId', (req, res) => {
+  const { ingredientName } = req.body;
+
+  con.query('SELECT IngredientID FROM ingredients WHERE IngredientName = ?', [ingredientName], (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error executing query');
+    } else {
+      res.json(result);
+    }
+  });
+});
+
 // uses the product ID to get the ingredients from the database
 router.post('/getIngredients', (req, res) => {
   const { productId } = req.body;
@@ -54,6 +95,51 @@ router.post('/getIngredients', (req, res) => {
       console.log('Ingredients:', ingredients);
       res.json(ingredients);
     });
+  });
+});
+
+// adds a productid with a corresponding ingredientid to product_ingredients
+router.post('/addIngredientToProduct', (req, res) => {
+  const { ProductID, IngredientID } = req.body;
+  // console.log('Product ID:', product, 'Ingredient ID:', ingredient);
+  
+  con.query('INSERT INTO product_ingredients (ProductID, IngredientID) VALUES (?, ?)', [ProductID, IngredientID], (err, result) => {
+  if (err) {
+    console.error(err);
+    res.status(500).send('Error adding ingredient to product');
+  } else {
+    console.log('Ingredient added to product:', result);
+    res.json({ success: true, result });
+  }
+  });
+});
+
+// removes a productid along with a corresponding ingredientid from product_ingredients
+router.post('/removeIngredientFromProduct', (req, res) => {
+  const { ProductID, IngredientID } = req.body;
+
+  con.query('DELETE FROM product_ingredients WHERE ProductID = ? AND IngredientID = ?', [ProductID, IngredientID], (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error removing ingredient from product');
+    } else {
+      console.log('Ingredient removed from product', result);
+      res.json({ success: true, result });
+    }
+  });
+});
+
+router.post('/insertNewIngredient', (req, res) => {
+  const { IngredientID, IngredientName } = req.body;
+
+  con.query('INSERT INTO ingredients (IngredientID, IngredientName) VALUES (?,?)', [IngredientID, IngredientName], (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error inserting new ingredient');
+    } else {
+      console.log('New Ingredient successfully added', result);
+      res.json({ success: true, result });
+    }
   });
 });
 
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# samSuppSense
+# COMPX241-Group Project : Smoke and Mirrors
+# SuppSense
+
+<p> Author : Damion Thomas Sklenars-Clare | 1638052 </p>
+<p> Author : Jeff Jia | </p>
+<p> Author : Navraj Singh Mann | </p>
+<p> Author : Shokei Leger | </p>
+
+<p> Course : COMPX241 - Data Structures and Algorithms </p>
